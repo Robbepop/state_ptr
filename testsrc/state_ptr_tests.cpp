5618e03c37073de5d4b01cbfa1b411ff3e81db96
Hero Bird <robbepop@web.de>
2017-08-20 19:17:19 +0200

    added state_ptr tests for operator*, operator->, operator bool as well as a temporary regression test for GitHub issue #4

--- testsrc/state_ptr_tests.cpp
+++ testsrc/state_ptr_tests.cpp
@@ -8,6 +8,7 @@ using namespace putl;
 
 struct Foo{
 	int a;
+	auto get_a() const noexcept -> int { return a; }
 };
 
 enum Bar {
@@ -113,5 +114,50 @@ TEST(StatePointer, EqualityPtrAndState) {
 	EXPECT_NE(p11, p22);
 }
 
+TEST(StatePointer, DerefOp) {
+	int foo1{42};
+	int foo2{1337};
+	state_ptr<int> p1a{&foo1, 1};
+	state_ptr<int> p1b{&foo1, 1};
+	state_ptr<int> p2 {&foo2, 1};
+	EXPECT_EQ(*p1a, *p1b);
+	EXPECT_NE(*p1a, *p2);
+	EXPECT_NE(*p1b, *p2);
+}
+
+TEST(StatePointer, ArrowOp) {
+	Foo foo1{42};
+	Foo foo2{1337};
+	state_ptr<Foo> p1a{&foo1, 1};
+	state_ptr<Foo> p1b{&foo1, 1};
+	state_ptr<Foo> p2 {&foo2, 1};
+	EXPECT_EQ(p1a->get_a(), p1b->get_a());
+	EXPECT_NE(p1a->get_a(), p2->get_a());
+	EXPECT_NE(p1b->get_a(), p2->get_a());
+}
+
+TEST(StatePointer, ImplicitBoolConversion) {
+	int foo{42};
+	state_ptr<int> p1{&foo, 1};
+	state_ptr<int> p2{nullptr, 1};
+	EXPECT_TRUE(p1);
+	EXPECT_FALSE(p2);
+}
+
+TEST(StatePointer, RegressionGitHubIssue4) {
+	// Compile-time regression where apple-clang reported the following error:
+	// 
+	// [...]/state_ptr/include/putl/state_ptr.hpp:263:10: error: reinterpret_cast from rvalue to reference type
+	//       'reference_type' (aka 'int &')
+	//                 return reinterpret_cast<reference_type>(get_ptr());
+	//                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// [...]/state_ptr/testsrc/state_ptr_tests.cpp:119:18: note: in instantiation of member function
+	//       'putl::state_ptr<int, unsigned long, 2>::operator*' requested here
+	//     int target = *underTest;
+	int testData = 1;
+	putl::state_ptr<int> underTest{&testData, 0};
+	int target = *underTest;
+	ASSERT_EQ(target, 1);
+}
 
 } // namespace
